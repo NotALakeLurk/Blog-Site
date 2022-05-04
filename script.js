cb4443280c35fc35ed366af73fc81e9245f328ee
NotALakeLurk <101846294+NotALakeLurk@users.noreply.github.com>
2022-05-04 14:21:56 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 //Functions
 
 //generate a random rgb color
-function RandColor(id)
+function RandLightColor()
 {
 	let rgb = new Array(3); //create an array to store the r, g, & b values
 
   //generate a random integer between 0 and 255 for each value
 	for(let i = 0; i < 3; i++)
 	{
-		rgb[i] = Random(255);
+		rgb[i] = Random(126, 255);
 	}
-
-	document.getElementById(id).style.color = `rgb(${rgb})`; //set the color of the element
+	let articles = document.querySelectorAll('article');
+	for(let article in articles)
+	{
+		document..style.color =`rgb(${rgb})`;
+	}
+	alert
 }
 
 //generate a random number from 0 to max
-function Random(max)
+function Random(min, max)
 {
-  return(Math.floor(Math.random() * (max + 1))); //random returns decimal 0-1, multiply by max and floor for integer
+	result = Math.floor(Math.random() * (max - min + 1)) + min; //random returns number 0-1,  
+	return(result);
 }
